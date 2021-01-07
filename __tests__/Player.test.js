33016f9cb49392f8255cd2d1f344b905379d01a0
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2021-01-06 22:53:34 -0500

    added tests for attack value, adding potions to inventory, using potion from inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,6 +3,7 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion.js');
 
 const Player = require('../lib/Player');
+const { TestScheduler } = require('jest');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -62,6 +63,37 @@ test("gets player's stats as an object", () => {
       player.reduceHealth(99999);
 
       expect(player.health).toBe(0);
+  });
+
+  test("gets player's attack value", () => {
+      const player = new Player('Dave');
+      player.strength = 10;
+
+      expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+      expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+  });
+
+  test('adds a potion to the inventory', () => {
+      const player = new Player('Dave');
+      const oldCount = player.inventory.length;
+
+      player.addPotion(new Potion());
+
+      expect(player.inventory.length).toBeGreaterThan(oldCount);
+  });
+
+  test('uses a potion from inventory', () => {
+      const player = new Player('Dave');
+      player.inventory = [new Potion(), new Potion(), new Potion()];
+      const oldCount = player.inventory.length;
+
+      player.usePotion(1);
+
+      expect(player.inventory.length).toBeLessThan(oldCount);
   })
+
+
+
+
 
 
